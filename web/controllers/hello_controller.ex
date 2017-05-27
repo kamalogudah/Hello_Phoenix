f488ac37435f7a1e1b6b8b8b54158818e849661d
kamalogudah <mcpaul2058@gmail.com>
2017-05-28 01:01:13 +0300

    Initial Commit

---
+++ web/controllers/hello_controller.ex
@@ -0,0 +1,10 @@
+defmodule HelloPhoenix.HelloController do
+  use HelloPhoenix.Web, :controller
+
+  def world(conn,%{"name" => name}) do
+    render conn, "world.html", name: name
+  end
+  
+end
+
+
